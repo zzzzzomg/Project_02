23bca6fdfd589022661bca5e4dbe05332a2b47ff
Kevin Chua <zzzzzomg@gmail.com>
2013-06-24 22:19:50 -0700

    lol

--- Week 3 - Test/nav.js
+++ Week 3 - Test/nav.js
@@ -1,11 +1,19 @@
 $(document).ready(function()  {
     $('#pull').click(function() {
-        $('nav ul').slideToggle('slow');
-        
-        
+        $('nav ul').slideToggle('slow');  
     });
-    
-    
 });
+
+
+$(document).ready(function()  {
+    $('#searchbutton').click(function() {
+        $('#search').slideToggle('slow');  
+    });
+});
+    
+
+    
+
+
     
     
